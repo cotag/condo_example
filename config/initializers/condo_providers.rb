5305546824a54f50465b608524bb4d12a7fc2c0c
Steve von Takach <steve@advancedcontrol.com.au>
2012-12-03 14:58:32 +1100

    Added config for Google v2 API

--- config/initializers/condo_providers.rb
+++ config/initializers/condo_providers.rb
@@ -26,6 +26,18 @@ Condo::Configuration.add_residence(:AmazonS3, {
 
 
 #
+# Enable this if you would like to use v2 of Google's storage API (https://developers.google.com/storage/docs/accesscontrol#Signed-URLs)
+# => Convert cert to PEM: openssl pkcs12 -in file/name.p12 - nodes -nocerts > out/put.pem
+# => NOTE:: The password is: notasecret
+#
+#Condo::Configuration.add_residence(:GoogleCloudStorage, {
+#	:access_id => ENV['GOOGLE_KEY'],			# Service account email
+#	:secret_key => File.read('google.pem'),		# Private key in pem format (don't use this location ;)
+#	:api => 2
+#})
+
+
+#
 # NOTE:: Whilst this provider could technically work,
 #		Rackspace currently ignores preflight requests so this doesn't work at all
 #
